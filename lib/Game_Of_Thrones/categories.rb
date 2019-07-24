78ecd66bab3de89d72b539805ba6721d2de85015
Peter Grant <jgrantps@hotmail.com>
2019-07-24 09:53:07 +0000

    return toilet names and prices

--- lib/Game_Of_Thrones/categories.rb
+++ lib/Game_Of_Thrones/categories.rb
@@ -1,23 +1,30 @@
 class GameOfThrones::Categories
   attr_accessor :name, :url
 
-  @subpage = ""
-  @subcategories = ""
 
    def initialize(url = "", name = "")
      puts "hello from subcategories!!"
+     @subpage = ""
+     @subcategories = ""
+     @subhash = {}
      @url = url
      @name = name
+
      subcategory_scraper
      puts "the name of this object is #{@name}"
      puts "Choose from the following Toilets:"
-     subcategory_list
+     subcategory_scraper
+    #  subcategory_list
    end
 
    def subcategory_scraper
-     @subpage = Nokogiri::HTML(open(@url))
-     @subcategories = @subpage.css("div.col-4-lg.col-6-md.col-6-sm.product-panel.product-panel-height-new")
-    #  binding.pry
+     @toilet_url = []
+     subpage = Nokogiri::HTML(open(@url))
+     subproducts = subpage.css("div.col-4-lg.col-6-md.col-6-sm.product-panel.product-panel-height-new")
+     @toilet_url = subproducts.collect {|t| "https://www.us.kohler.com"+t.css("a").attr("href").value}
+     @toilet_names = subproducts.collect {|t| t.css("p.product-panel__summary.product-panel__summary-new").text}
+     @toilet_prices = subproducts.collect {|t| t.css("p.product-panel__price.product-panel__price-new.light-gray--sku--price").text.gsub("Starting at ","").strip}#.gsub("\n\t\t\tStarting at \n\t\t\t\t\n\t\t\t", "")}
+     binding.pry
    end
 
    def subpage
@@ -29,7 +36,7 @@ class GameOfThrones::Categories
    end
 
    def subcategory_list
-
+     @subhash = {}
    end
 
 
