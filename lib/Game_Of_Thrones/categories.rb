b2ad53ea0a050014e8d84e5cf5aa763e393adc7c
Peter Grant <jgrantps@hotmail.com>
2019-07-25 17:07:20 +0000

    backed out object instantiator

--- lib/Game_Of_Thrones/categories.rb
+++ lib/Game_Of_Thrones/categories.rb
@@ -18,19 +18,19 @@ class GameOfThrones::Categories
      @toilet_url = []
      subpage = Nokogiri::HTML(open(@url))
      subproducts = subpage.css("div.col-4-lg.col-6-md.col-6-sm.product-panel.product-panel-height-new")
-     count = 1
-     subproducts.each do |t| #=> repair for creating individual toilets.
-       binding.pry
-       GameOfThrones::Thrones.new(t.css("p.product-panel__summary.product-panel__summary-new").text, t.css("p.product-panel__price.product-panel__price-new.light-gray--sku--price").text.gsub("Starting at ","").strip, t.css("https://www.us.kohler.com"+t.css("a").attr("href").value.gsub("s.jsp?productId=","/toilets/").gsub("?",".htm?")), count)
-       count += 1
-     end
-     binding.pry
+    #  count = 1
+    #  subproducts.each do |t| #=> repair for creating individual toilets.
+    #    binding.pry
+    #    GameOfThrones::Thrones.new(@toilet_names, @toilet_prices, @toilet_url, count)
+    #    count += 1
+    #  end
      @toilet_url = subproducts.collect {|t| "https://www.us.kohler.com"+t.css("a").attr("href").value.gsub("s.jsp?productId=","/toilets/").gsub("?",".htm?")}
      @toilet_names = subproducts.collect {|t| t.css("p.product-panel__summary.product-panel__summary-new").text}
      @toilet_prices = subproducts.collect {|t| t.css("p.product-panel__price.product-panel__price-new.light-gray--sku--price").text.gsub("Starting at ","").strip}
+     binding.pry
 
     #  binding.pry
-    guess
+    # guess
    end
 
    def toilet_url
