172f5deb994757ff99d6d15f50ea9859697263bb
Peter Grant <jgrantps@hotmail.com>
2019-07-27 15:03:23 +0000

    category.input_check is operating with proper conditions - complete routing

--- lib/Game_Of_Thrones/categories.rb
+++ lib/Game_Of_Thrones/categories.rb
@@ -7,7 +7,7 @@ class GameOfThrones::Categories
      @name = name
      @index = index
      @toilets = []
-     @displayed_index = []
+     @displayed_toilets = []
      @@all<<self
    end
 
@@ -16,7 +16,7 @@ class GameOfThrones::Categories
    end
 
    def thrones_scraper
-#=> scrape the category for its corresponding toilets.
+#=> scrapes the category for its corresponding toilets.
      subpage = Nokogiri::HTML(open(@url))
      subproducts = subpage.css("div.col-4-lg.col-6-md.col-6-sm.product-panel.product-panel-height-new")
       count = 1
@@ -30,33 +30,40 @@ class GameOfThrones::Categories
    def guess #=> Prompts user for selection
      puts "\n--> To guess which throne you think is the most expensive, enter the # followed by a '!'"
      puts "--> To find out more info on each throne, enter the # followed by a '?'\n\n"
-    guess_display #=> displays toilet table for user to make an decision over.
-    input = gets.strip
-    input_check(input) #=> returns response based on if there is request for more details, a guess is made, or input is an error.
+    selection_generator #=> displays toilet table for user to make an decision over.
+    # input = gets.strip
+    input_check #=> returns response based on if there is request for more details, a guess is made, or input is an error.
   end
 
-   def guess_display #=> returns the list of 3 random toilets to choose from.
+   def selection_generator #=> returns the list of 3 random toilets to choose from.
 
      self.toilets.sample(3).each do |toilet|
        puts "   #{toilet.index}. #{toilet.name}, #{toilet.price}"
-       @displayed_index << toilet.index.to_s
+       @displayed_toilets << toilet #=> stores the generated selection in a dedicated array.
      end
-binding.pry
+# binding.pry
   end
 
-  def input_check(var) #=> routes the program to the appropriate method based on the var argument.
-    #if var == #the top price in the name_price_hash
-      if @displayed_index.include? var.split("").first #=> checks if the input number is a match for any toilet index in the guess_display.
+  def input_check #=> routes the program to the appropriate method based on the var argument.
+    var = gets.strip
+
+    a = @displayed_toilets.map {|i| i.index.to_s }.include? var.split("").first
+    b = (var.split("").last == "!")
+    c = (var.split("").last == "?")
+
+      # if (@displayed_toilets.map {|i| i.index.to_s }.include? var.split("").first && ((var.split("").last == "!") || (var.split("").last == "?")) == true)#=> checks if the input number is a match for any toilet index in the guess_display.
+      if a && (b || c) == true
+        # binding.pry
+
         puts "Your input is valid!"
-       if var.split("").last == "!"
-      #   GameOfThrones::SubCategories.new(url)
-      #   puts "you entered in #{var} with an exclamation point"
-      # elsif var.split("").last == "?"
-      #   puts "you entered in #{var} with a question mark"
-      else
+      #  if var.split("").last == "!" #=> returns responese for if the user makes a guess.
+      #    #=> returns the toilet.index, name, and price for the toilet seleted.
+      #  if var.split("").last == "?" #=> returns response ofr if the user asks for more information.
+      #    #=> checks to see whether the toilet selected in the input is the most expensive.
+      else #=> returns error message and prompts user to retry.
+        # binding.pry
         puts "you entered a typo - try again!"
-      guess
+        input_check
       end
   end
-
 end
